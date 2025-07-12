d46ca24852133fa1ee1bb456396323f86d330d44
aseel332 <lakadia@ucdavis.edu>
2025-07-12 15:22:15 +0530

    CheckPoint: Admin Home Layout Progressed

---
+++ avenger-web/components/MissionsGlobe.jsx
@@ -0,0 +1,13 @@
+export default function MissionsGlobe(){
+
+  const header = (<header className="title-container">
+    <p className="red-title">Missions</p>
+    <hr className="red-line" />
+  </header>);
+
+  return(
+    <>
+    {header}
+    </>
+  )
+}
--- avenger-web/components/PostLayoutAdmin.jsx
+++ avenger-web/components/PostLayoutAdmin.jsx
@@ -1,7 +1,9 @@
+import MissionsGlobe from "./MissionsGlobe"
+
 export default function PostLayoutAdmin(){
   const header = (
-    <div className="header-container">
-      <button className='sidebar-button'>
+      <>
+      <button className='mail-button'>
         <i className="fa-solid fa-bars"></i>
       </button>
       <span className="header-avenger">Avengers</span>
@@ -12,11 +14,17 @@ export default function PostLayoutAdmin(){
       <span className='profile-pic'> <i className="fa-solid fa-user"></i> </span>
       <span>Sam, W.</span>
       </div>
-    </div>
+      </>
+
   )
   return(
     <>
+    <div className="header-container">
     {header}
+    </div>
+    <div className="body-card">
+    {<MissionsGlobe />}
+    </div>
     </>
   )
 }
