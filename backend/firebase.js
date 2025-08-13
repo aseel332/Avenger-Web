0bf66990a8c7a0210ee0d7e3396fde945037559e
aseel332 <lakadia@ucdavis.edu>
2025-08-13 16:20:53 +0530

    gotta remove the dot

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./etc/secrets/avenger-canvas-firebase-adminsdk-fbsvc-038deb9a19.json");
+const serviceAccount = require("/etc/secrets/avenger-canvas-firebase-adminsdk-fbsvc-038deb9a19.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
